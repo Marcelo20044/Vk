347a9962c6b0c0a4cc1972d52ecb21f0906a48e9
Khvostenko <khmark04@mail.ru>
2021-11-05 01:59:22 +0700

    initial commit

---
+++ src/main/java/domain/PostVk.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class PostVk {
+
+    private int id;
+    private int ownerId;
+    private int publicationDate;
+    private String text;
+    private Object likes;
+    private Object comments;
+    private Object reposts;
+    private Object views;
+}
